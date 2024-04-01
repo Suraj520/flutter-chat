f3d036160cd32e5111d976db80e937e8d0697e24
Suraj5 <suraj.suraj.4.4.1996@gmail.com>
2024-04-01 06:27:19 +0000

    socket io:: initial snippets inclusion

--- lib/src/pages/group/home/home_page.dart
+++ lib/src/pages/group/home/home_page.dart
@@ -1,12 +1,17 @@
+// home_page.dart
 import 'package:flutter/material.dart';
+import 'package:socketio_flutter/src/pages/chat/chat_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,15 +20,52 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: TextButton(
-            onPressed: showDialog(context: context, builder: builder),
-            child: const Text(
-              "Initiate Team Chat",
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 16,
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: const Text("Please Enter your name"),
+                content: TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your name',
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      if (_nameController.text.isNotEmpty) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChatPage(
+                              username: _nameController.text,
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text("Enter"),
+                  )
+                ],
               ),
-            )),
+            );
+          },
+          child: const Text(
+            "Initiate Team Chat",
+            style: TextStyle(
+              color: Colors.teal,
+              fontSize: 16,
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
   }
 }
